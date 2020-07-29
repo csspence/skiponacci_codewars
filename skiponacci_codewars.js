bb44e102b3cef883c4684d3b4d94f509734af459
Crew Spence <csspence123@gmail.com>
2020-07-29 07:17:57 -0500

    Wrote working solution.

--- skiponacci_codewars.js
+++ skiponacci_codewars.js
@@ -8,6 +8,26 @@ Return the result as a string
 You can presume that n is always a positive integer between (and including) 1 and 64.
 */
 
-function skiponacci(n) {
+const skiponacci = (n) => {
+  let str = '';
+  let toggle = false;
+  let fibonacci = [0, 1, 1];
+  for(let i = 3; i < 65; i++) {
+    fibonacci.push(fibonacci[fibonacci.length - 1] + fibonacci[fibonacci.length - 2]);
+  }
+  for(let i = 1; i < n + 1; i++) {
+    if(i === 1 && n === 1) {
+      return '1';
+    }
+    if(i === 1){
+      str += i;
+      continue
+    }
+    if(toggle === true) {
+      str += ' skip ' + fibonacci[i];
+    }
+    toggle = !toggle;
+  }
 
+  return str;
 }
